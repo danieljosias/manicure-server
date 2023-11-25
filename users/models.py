6005e10322c1491546fd6491366542c2ee165650
Daniel Josias <daniieljosiaslima@gmail.com>
2023-11-24 23:20:31 -0300

    feat app users

--- users/models.py
+++ users/models.py
@@ -4,6 +4,9 @@ import uuid
 
 
 class User(AbstractUser):
-    id = models.UUID(default=uuid.uuid4, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+
+
+
