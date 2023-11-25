c1d85ade4dcdbf43222c7e88731ffde5e9bbeadd
Daniel Josias <daniieljosiaslima@gmail.com>
2023-11-24 21:22:29 -0300

    feat app users

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class User(AbstractUser):
+    id = models.UUID(default=uuid.uuid4, primary_key=True, editable=False)
+    username = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
