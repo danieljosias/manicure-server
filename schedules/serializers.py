6d8567fc77ec4987cb3854ec61de14e9b0e225b6
Daniel Josias <daniieljosiaslima@gmail.com>
2023-11-30 13:52:57 -0300

    feat: app de agendamentos

--- schedules/serializers.py
+++ schedules/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Schedule
 from users.models import User
-from users.serializers import UserSerializer
 from django.shortcuts import get_object_or_404
 
 
